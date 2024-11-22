1abea9a5c201d988ff4da1bee09f88f6344ea4ce
3kmek <78922332+3kmek@users.noreply.github.com>
2024-11-23 00:10:29 +0300

    conveyor belt calisiyor simdilik

---
+++ Assets/Ali Emir/Scripts/ConveyorBelt.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour
+{
+    [SerializeField] private float speed; // Kaydırma hızı
+    [SerializeField] private Material material; // Conveyor belt'in malzemesi
+
+    private Vector2 offset; // Texture kaydırma için vektör
+
+    void Start()
+    {
+        // MeshRenderer'ın materialine eriş
+        if (material == null)
+        {
+            material = GetComponent<MeshRenderer>().material;
+        }
+
+        // Başlangıç kaydırma vektörünü sıfırla
+        offset = Vector2.zero;
+    }
+
+    void Update()
+    {
+        // Y ekseni boyunca kaydırma
+        offset.y -= speed * Time.deltaTime;
+
+        // Malzeme üzerindeki texture kaydırmasını uygula
+        material.mainTextureOffset = offset;
+    }
+}
