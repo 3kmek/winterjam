d498084af1f2b5f8844eceaa1847fbd8bb45d8d5
emyrysf <bolhocaliogluemiryusuf@gmail.com>
2024-11-23 12:42:07 +0300

    emirin ucube her sey karısık commiti

---
+++ Assets/Emir Yusuf/Scripts/Product.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Product : MonoBehaviour
+{
+    public GameObject[] Resources ;
+    public string Name;
+}
