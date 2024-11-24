69b2d407891361922aa6cdad0de29cddd26aaca7
3kmek <78922332+3kmek@users.noreply.github.com>
2024-11-24 15:47:37 +0300

    ab

--- Assets/Ali Emir/Objeler/Dolap/Dolap.cs
+++ Assets/Ali Emir/Objeler/Dolap/Dolap.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class Dolap : MonoBehaviour
 {
-    [SerializeField] private GameObject LeftEl, RightEl, LeftLeg, RightLeg;
+    [SerializeField] private GameObject LeftEl, RightEl, LeftLeg, RightLeg, Body, Head;
 
     [SerializeField] public int DolabaAtilan = 0;
     // Start is called before the first frame update
@@ -24,31 +24,76 @@ public class Dolap : MonoBehaviour
     // Update is called once per frame
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.GetComponent<LEl>())
-        {
-            other.gameObject.SetActive(false);
-            LeftEl.SetActive(true);
-            DolabaAtilan += 1;
-        }
+        
         
         if (other.gameObject.GetComponent<REl>())
         {
-            other.gameObject.SetActive(false);
-            RightEl.SetActive(true);
-            DolabaAtilan += 1;
+            if (RightEl.activeSelf)
+            {
+                other.gameObject.SetActive(false);
+                RightEl.SetActive(true);
+                DolabaAtilan += 1;
+            }
+
+            else
+            {
+                other.gameObject.SetActive(false);
+                LeftEl.SetActive(true);
+                DolabaAtilan += 1;
+            }
+            
         }
         
         if (other.gameObject.GetComponent<RightLeg>())
         {
+            
+            if (RightLeg.activeSelf)
+            {
+                other.gameObject.SetActive(false);
+                DolabaAtilan += 1;
+                LeftLeg.SetActive(true);
+            }
+            else
+            {
+                other.gameObject.SetActive(false);
+                RightLeg.SetActive(true);
+                DolabaAtilan += 1;
+            }
+        }
+        
+        
+        if (other.gameObject.GetComponent<RightLeg>())
+        {
+            
+            if (RightLeg.activeSelf)
+            {
+                other.gameObject.SetActive(false);
+                DolabaAtilan += 1;
+                LeftLeg.SetActive(true);
+            }
+            else
+            {
+                other.gameObject.SetActive(false);
+                RightLeg.SetActive(true);
+                DolabaAtilan += 1;
+            }
+        }
+        
+        
+        
+       
+        
+        if (other.gameObject.GetComponent<Head>())
+        {
             other.gameObject.SetActive(false);
-            RightLeg.SetActive(true);
+            Head.SetActive(true);
             DolabaAtilan += 1;
         }
         
-        if (other.gameObject.GetComponent<LeftLeg>())
+        if (other.gameObject.GetComponent<Body>())
         {
             other.gameObject.SetActive(false);
-            LeftLeg.SetActive(true);
+            Body.SetActive(true);
             DolabaAtilan += 1;
         }
     }
