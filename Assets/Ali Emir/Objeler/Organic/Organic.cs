2f4745c187a864ce4f71688c00a538b718e93583
3kmek <78922332+3kmek@users.noreply.github.com>
2024-11-24 12:38:37 +0300

    Organik skor sayımı

--- Assets/Ali Emir/Objeler/Organic/Organic.cs
+++ Assets/Ali Emir/Objeler/Organic/Organic.cs
@@ -1,55 +1,40 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class Dolap : MonoBehaviour
+public class Organic : MonoBehaviour
 {
-    [SerializeField] private GameObject LeftEl, RightEl, LeftLeg, RightLeg;
-
-    [SerializeField] public int DolabaAtilan = 0;
+    [SerializeField] public int OcagaAtılan = 0;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-
-    private void Update()
-    {
-        
-    }
-
-    // Update is called once per frame
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.GetComponent<LEl>())
         {
             other.gameObject.SetActive(false);
-            LeftEl.SetActive(true);
-            DolabaAtilan += 1;
+            OcagaAtılan +=1 ;
         }
         
         if (other.gameObject.GetComponent<REl>())
         {
             other.gameObject.SetActive(false);
-            RightEl.SetActive(true);
-            DolabaAtilan += 1;
+            OcagaAtılan +=1 ;
         }
         
         if (other.gameObject.GetComponent<RightLeg>())
         {
             other.gameObject.SetActive(false);
-            RightLeg.SetActive(true);
-            DolabaAtilan += 1;
+            OcagaAtılan +=1 ;
         }
         
         if (other.gameObject.GetComponent<LeftLeg>())
         {
             other.gameObject.SetActive(false);
-            LeftLeg.SetActive(true);
-            DolabaAtilan += 1;
+            OcagaAtılan +=1 ;
         }
     }
 }
